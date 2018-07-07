787174f55d9a2da4347ac875e6a35f9bca814b7e
Abhishek Chakraborty <eofsentinel@outlook.com>
2018-07-07 12:06:01 +0530

    solved Euler #1

--- Project Euler/euler001.cpp
+++ Project Euler/euler001.cpp
@@ -30,12 +30,12 @@ int main(){
     for(int a0 = 0; a0 < t; a0++){
         int n;
         cin >> n;
-        int c3 = (n-1) / 3;
-        int c5 = (n-1) / 5;
-        int c15 = (n-1) / 15;
-        int s3 = c3/2.0 * (2*3 + (c3-1)*3);
-        int s5 = c5/2.0 * (2*5 + (c5-1)*5);
-        int s15 = c15/2.0 * (2*15 + (c15-1)*15);
+        long long c3 = (n-1) / 3;
+        long long c5 = (n-1) / 5;
+        long long c15 = (n-1) / 15;
+        long long s3 = (c3 * (2*3 + (c3-1)*3)) / 2;
+        long long s5 = (c5 * (2*5 + (c5-1)*5)) / 2;
+        long long s15 = (c15 * (2*15 + (c15-1)*15)) / 2;
         cout << s3 + s5 - s15 << endl;
     }
     return 0;
